f687eeff70bc30c640a1b50d730a4e57a69b0c94
taylor-green <taylor.green2804@gmail.com>
2023-04-14 10:37:24 -0400

    completed routes in api.js

--- Develop/public/assets/js/api.js
+++ Develop/public/assets/js/api.js
@@ -16,6 +16,20 @@ router.post('/', (req, res) => {
             id: uuid.v4(),
             title: newTitle,
             text: newText
-        }
-    }
-})
+        };
+        data.push(newJSON);
+        res.json(data);
+    };
+});
+
+router.delete('/:id', (req, res) => {
+    const found = data.some(obj => obj.id === req.params.id);
+    if (found) {
+        data = data.filter(obj => obj.id !== req.params.id);
+        res.json(data);
+    } else {
+        res.status(400).json(data);
+    };
+});
+
+    module.exports = router;
